8654b31549811b000e6132ccdcce7a33bc4fb902
Dillon Jones <dillonj4682@gmail.com>
2026-08-27 03:05:12 -0400

    AudioTrack Object

---
+++ src/main/java/audiana/AudioTrack.java
@@ -0,0 +1,25 @@
+package audiana;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioHeader;
+
+public class AudioTrack {
+    private AudioFile audioFile;
+    private AudioHeader header;
+    public String name;
+
+    public AudioTrack(AudioFile audioFile) {
+        this.audioFile = audioFile;
+        this.header = audioFile.getAudioHeader();
+        this.name = audioFile.getFile().getName();
+    }
+
+    public void DisplayInfo() {
+        System.out.println("\n" + "-- Audio Information -- ");
+        System.out.println("File Name:   " + name);
+        System.out.println("Duration:    " + header.getTrackLength() + " seconds");
+        System.out.println("File Size:   " + audioFile.getFile().length() + " bytes");
+        System.out.println("Bitrate:     " + header.getBitRate());
+        System.out.println("Sample Rate: " + header.getSampleRateAsNumber() + " Hz");
+        System.out.println("Channels:    " + header.getChannels());
+    }
+}
