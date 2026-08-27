17ece30937512ac253331a47b76292a830ec90a2
Dillon Jones <dillonj4682@gmail.com>
2026-08-26 22:52:09 -0400

    File Validation

--- src/main/java/audiana/Main.java
+++ src/main/java/audiana/Main.java
@@ -1,4 +1,5 @@
 package audiana;
+import java.io.File;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -11,12 +12,24 @@ public class Main {
         /_/   §_§§__,_|§__,_|_|§__,_|_| |_|§__,_| """.replace("§", "\\");
 
         System.out.println(audianaLogo + " " + versionNumber + "\n");
-
         Scanner scanner = new Scanner(System.in);
+        File file;
 
-        System.out.print("Enter The File Path (mp3, wav, ogg): ");
-        String input = scanner.nextLine();
+        do { 
+            System.out.print("Enter The File Path (mp3, wav, ogg): ");
+            String path = scanner.nextLine();
+            file = new File(path);
 
-        System.out.println("Loading file from " + input + "...");
+            System.out.println("Loading file from " + path + "...");
+
+            if(file.exists() && file.isFile()) { 
+                System.out.println("File Loaded! "); 
+            }
+            else { 
+                System.out.println("ERROR: File at PATH is not compatible or doesnt exist "); 
+            }
+        } while (!file.exists() || !file.isFile());
     }
 }
+
+//java -cp target/classes audiana.Main
