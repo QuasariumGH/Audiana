04232c721413d6bdd5d024c6709ff472fe578260
Dillon Jones <dillonj4682@gmail.com>
2026-08-28 01:05:35 -0400

    Playlist Object

---
+++ src/main/java/audiana/Playlist.java
@@ -0,0 +1,17 @@
+package audiana;
+
+public class Playlist {
+
+    public AudioTrack[] files;
+    public int size;
+
+    public Playlist(AudioTrack[] files, int size)
+    {
+        this.files = files;
+        this.size = size;
+    }
+    public Playlist sort() {
+        Playlist playlist = null;
+        return playlist;
+    }
+}
